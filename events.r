a64ce98cdd3128455ac85fcb34c86955eb42a443
Ahern Nelson <ahernnelson4@gmail.com>
2018-03-15 16:15:54 -0600

    Added imputed data and diagnostic plots

--- events.r
+++ events.r
@@ -1,13 +1,20 @@
-library(ggplot2)
-library(dplyr)
-library(lubridate)
+library(plotly)
 library(pacman)
-p_load("tseries", "xts", "forecast", "astsa", "zoo", "forecast")
+p_load("tseries", "xts", "forecast", "astsa", "zoo", "forecast", 
+       "tidyverse", "gridExtra", "lubridate", "mice", "car", "rgl",
+       "zoo", "xts", "forecast")
 # IMPORTANT IMPORTANT IMPORTANT
 TREU = TRUE
 # IMPORTANT IMPORTANT IMPORTANT
-
-bear <- read.csv("trimmed2017.csv",header=TRUE)
+#### impute ############################
+to_imput <- read.csv("trimmed2017.csv")
+tmpImp <- data.frame(to_imput$e.coli, to_imput$tempC)
+imputed <- mice(tmpImp ,m = 1 ,maxit = 1 ,meth = 'pmm' ,seed = 500)
+imputed <- complete(imputed)
+to_imput$e.coli <- imputed$to_imput.e.coli
+to_imput$tempC <- imputed$to_imput.tempC
+########################################
+bear <- to_imput
 names(bear)[names(bear) == 'e.coli'] <- 'EColi'
 ord <-c("BCL1","BCL3","BCL4","WEC","BCL5","BC-Estes","BC-Wads","BCD1",
         "BC-Sher","BC-BCP","BCD2","BCD3","BCS1","BCS2","BCS3","BCS4",
@@ -69,20 +76,16 @@ ggplot(data=bear,aes(x=bear$daysFromOrigin, y=log(bear$EColi), col=Site)) +
   geom_jitter(alpha=.7) + facet_wrap(~geoBins,nrow=4) + 
   theme_bw()
 
-ggplot(data=bear[bear$geoBins!="binsHBC",],aes(x=Date, y=logEColi, col=Site)) + 
+p <- ggplot(data=bear[bear$geoBins!="binsHBC",],aes(x=Date, y=logEColi, col=Site)) + 
   geom_jitter(alpha=.7) + facet_wrap(~geoBins,nrow=4) + 
   geom_line(aes(y=bear[bear$geoBins!="binsHBC",]$tempC/3,x=Date)) +
   theme_bw()
-
 ggplot(data=bear[bear$geoBins=="binsLBC",],aes(x=Date, y=logEColi, col=Site)) + 
   geom_jitter(alpha=.7) + facet_wrap(~geoBins,nrow=4) + 
   theme_bw()
 
 #### We want to see if our trend persists after averaging over two week periods
 
-library(zoo)
-library(xts)
-library(forecast)
 
 bear %>% filter(geoBins=="binsLBC") -> bearLBC
 
@@ -94,7 +97,8 @@ bearLBC %>%
   group_by(week=floor_date(Date, "15 day")) %>% 
   summarize(medianLogEColi = median(logEColi, na.rm=TREU)) ->bearTSP
 
-bearTS <- xts(bearTSP$medianLogEColi, order.by=as.Date(bearTSP$week, "%m/%d/%Y"))
+
+bearTS <- xts(bearTSP$medianLogEColi[-70], order.by=as.Date(bearTSP$week, "%m/%d/%Y")[-70])
 bearTS <- ts(bearTS)
 adf.test(diff(log(na.omit(bear$EColi))), alternative="stationary", k=0)
 
@@ -118,12 +122,17 @@ pred <- predict(fitter, n.ahead = 100)
 ts.plot(bearTS,pred$pred, log = "y", lty = c(1,3))
 
 
-fit <- Arima(bearTS, order=c(2,0,1), xreg=fourier(bearTS,K=2))
+fit <- Arima(bearTS, order=c(2,0,1), xreg=fourier(bearTS, K = 2))
 
 heyo <- sarima(bearTS, 8,1,1,0,1,1,24)
 
 preds <- sarima.for(bearTS, n.ahead=3, 2,0,1,1,2,1,12)
 
+autoplot(forecast(fit))
+autoplot(fit$residuals)
+hist(fit$residuals)
+lines(seq(-3,3,.1), 50*dnorm(seq(-3,3,.1),0, sd(fit$residuals)), col = 2)
+qqnorm(fit$residuals)
 ## What if we ran the same model but with the sewer break elimnated?
 
 bearTSQ <- bearTSP
@@ -140,6 +149,11 @@ ts.plot(bearTS2,pred$pred,log='y', lty=c(1,3))
 
 heyo <- sarima(bearTS2, 4,0,1,1,1,0,24)
 
+autoplot(forecast(fit))
+autoplot(fit$residuals)
+hist(fit$residuals)
+lines(seq(-3,3,.1), 50*dnorm(seq(-3,3,.1),0, sd(fit$residuals)), col = 2)
+qqnorm(fit$residuals)
 # The model looks better in diagnostics, however:
 
 fit <- arima(bearTS2, 
@@ -148,6 +162,13 @@ pred <- predict(fit,n.ahead=140)
 ts.plot(bearTS2,pred$pred,log='y', lty=c(1,3))
 
 heyo <- sarima(bearTS2, 4,0,1,1,1,0,24)
+
+
+autoplot(forecast(fit))
+autoplot(fit$residuals)
+hist(fit$residuals)
+lines(seq(-3,3,.1), 50*dnorm(seq(-3,3,.1),0, sd(fit$residuals)), col = 2)
+qqnorm(fit$residuals)
 
 preds <- sarima.for(bearTS, n.ahead=14, 1,0,1,0,1,1,12)
 
